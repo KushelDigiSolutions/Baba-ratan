4cd19149f3851482e74a31156af8437e20bae103
harsh <harshpandey4489@gmail.com>
2026-04-25 13:36:17 +0530

    some changes in contact form

--- app/COMMON/ConsultantForm.jsx
+++ app/COMMON/ConsultantForm.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 export default function ConsultantForm({ onClose }) {
--- app/contact/ContactForm.jsx
+++ app/contact/ContactForm.jsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa"
